bf9e0f5d7a94e65fc16241ca41f0d19337524114
Станіслав Толстоноженко <stastolsto@gmail.com>
2019-04-26 14:35:02 +0300

    1

---
+++ Kursach/FormBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Kursach.Class;
+
+namespace Kursach
+{
+    partial class FormBase : Form
+    {
+        private static bool isInit = false;
+        public FormBase()
+        {
+            InitializeComponent();
+        }
+
+        public void AddFunction(string name, List<Value> results)
+        {
+            this.Text = name;
+            this.Size = new System.Drawing.Size(1000, 550);
+            Series series = new Series(name);
+            series.ChartType = SeriesChartType.Line;
+
+            ListBox listBox1 = new ListBox();
+            listBox1.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Bottom);
+            listBox1.Size = new System.Drawing.Size(340, 450);
+            listBox1.Location = new System.Drawing.Point(10, 50);
+            listBox1.Items.AddRange(results.Select(e => $"X = {e.x}, Y = {e.y}, Q = {e.q}").ToArray());
+            this.Controls.Add(listBox1);
+            
+            Chart chart1 = new Chart();
+
+            Legend legend1 = new Legend();
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+
+            ChartArea chartArea1 = new ChartArea();
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            chart1.TabIndex = 1;
+            chart1.Size = new System.Drawing.Size(620, 500);
+            chart1.Location = new System.Drawing.Point(380, 10);
+            results.ForEach(e => series.Points.AddXY(e.x, e.y));
+            chart1.Series.Add(series);
+            chart1.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left | AnchorStyles.Top);
+            this.Controls.Add(chart1);
+            isInit = true;
+            ShowDialog();
+        }
+
+        public void AddPicture(string name)
+        {
+            this.Name = name;
+            this.Size = new System.Drawing.Size(400, 180);
+
+            PictureBox pictureBox1 = new PictureBox();
+            //??неясно як буду передавать параметри
+            pictureBox1.Size = new System.Drawing.Size(380, 140);
+            pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.Controls.Add(pictureBox1);
+
+            isInit = true;
+        }
+    }
+}
---
+++ Kursach/FormMain.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kursach.Class;
+using System.Media;
+using WMPLib;
+
+namespace Kursach
+{
+    public partial class FormMain : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        private DataBase dataBase;
+        public FormMain()
+        {
+            InitializeComponent();
+            CreatePicture();
+        }
+        public void CreatePicture()
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Users\\stast\\Desktop\\123.png");
+        }
+
+        public bool Check()
+        {
+            if(string.IsNullOrWhiteSpace(textBoxXMin.Text) || string.IsNullOrWhiteSpace(textBoxXMax.Text) || string.IsNullOrWhiteSpace(textBoxDx.Text))
+            {
+                return false;
+            }
+            if (Convert.ToDouble(textBoxXMin.Text) > Convert.ToDouble(textBoxXMax.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBoxA.Text);
+            double xMin = Convert.ToDouble(textBoxXMin.Text);
+            double xMax = Convert.ToDouble(textBoxXMax.Text);
+            double dx = Convert.ToDouble(textBoxDx.Text);
+            dataBase = new DataBase(a,xMax,xMin,dx);
+            dataBase.Start(progressBar);
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != ',' && number != '-')
+            {
+                e.Handled = true;
+            }
+
+            if(number == ',' && ((sender as TextBox).Text.IndexOf(',')) > -1) {
+                e.Handled = true;
+            }
+            if (number == '-' && ((sender as TextBox).Text.IndexOf('-')) > -1)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void grafictToolStripMenuItemGraph1_Click(object sender, EventArgs e)
+        {
+            FormBase formGraf1 = new FormBase();
+            formGraf1.AddFunction(Lang.language.TextMenuF1,dataBase.GetResult(typeof(Funtion1)));
+        }
+
+
+
+
+
+
+
+
+
+        private void ukrainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("uk");
+            Refresh();
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("es");
+            Refresh();
+        }
+        private void Refresh()
+        {
+            labelNameStudent.Text = Lang.language.NameStudnet + Environment.NewLine + Lang.language.GroupStudent;
+            labelInputeData.Text = Lang.language.TextInputeData;
+            buttonStart.Text = Lang.language.TextButtomStart;
+            toolStripStatusLabelErrors.Text = Lang.language.TextError;
+            toolStripStatusLabelTime.Text = Lang.language.TextTimeWork;
+            ToolStripMenuItemF1.Text = Lang.language.TextMenuF1;
+            ToolStripMenuItemF2.Text = Lang.language.TextMenuF2;
+            ToolStripMenuItemGraphF1.Text = ToolStripMenuItemGraphF2.Text = Lang.language.TextMenuGraf;
+            ToolStripMenuItemFormulaF1.Text = ToolStripMenuItemFormulaF2.Text = Lang.language.TextMenuFormula;
+            languagesToolStripMenuItem.Text = Lang.language.TextMenuLang;
+            ukrainToolStripMenuItem.Text = Lang.language.TextMenuLangUkr;
+            englishToolStripMenuItem.Text = Lang.language.TextMenuLangEng;
+        }
+        private void ricardoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            labelNameStudent.Text = "Ricardo Milos";
+            pictureBox1.Image = Properties.Resources.ricardo1;
+            player.URL = "ricardo.mp3";
+            player.controls.play();
+        }
+    }
+}
